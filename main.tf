0d259eba102e0c3fe184d1a39d0e026de77ece47
Samkalp360 <samkalp360@outlook.com>
2025-08-14 08:01:46 +0530

    Update main.tf
    Project added

--- main.tf
+++ main.tf
@@ -10,3 +10,9 @@ terraform {
 provider "google" {
   
 }
+
+resource "google_project" "my_project_12th" {
+  name       = "My Project"
+  project_id = "your-project-09876"
+  org_id     = "1234567890"
+}
